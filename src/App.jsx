6f5fa511d935971712766e2914c0463499251800
Lucie Hélias <lucie.helias@protonmail.com>
2024-10-26 16:34:59 +0200

    create navBar component and add photo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import Navbar from "./components/Navbar";
+
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="overflow-x-hidden text-gray-800 antialiased slec:bg-violet-300 selection:text-violet-800">
+      <div className="flixed top-0 -z-10 h-full w-full"></div>
+      <div className="absolute top-0 -z-10 size-full bg-white ">
+        <div className="absolute bottom-auto left-auto right-0 top-0 size-[500px] translate-x-[-30%] translate-y-[20%] rounded-full bg-[rgba(173,109,244,0.5)] opacity-50 blur-[80px]" />
+      </div>
+      <div className="container mx-auto px-8">
+        <Navbar />
+      </div>
     </div>
   );
 };
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+
+import logo from "../assets/images/Lucie Hélias.jpg";
+
+const Navbar = () => {
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6 ">
+      <div className="flex flex-shrink-0 items-center">
+        <img src={logo} alt="logo" className="h-12 w-12 rounded-full" />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <FaGithub />
+        <FaLinkedin />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
