35cf4e032bba37da3de05c36aedc8e742f38078e
Lucie Hélias <lucie.helias@protonmail.com>
2024-10-28 16:28:42 +0100

    add env

--- src/assets/components/Contact.jsx
+++ src/assets/components/Contact.jsx
@@ -17,7 +17,7 @@ const Contact = () => {
 
     setResult("Envoi en cours...");
     const formData = new FormData(event.target);
-    formData.append("access_key", "dbe5b28d-d490-45ce-9d5e-dc3d4bb816d4");
+    formData.append("access_key", import.meta.env.VITE_API_KEY);
 
     const response = await fetch("https://api.web3forms.com/submit", {
       method: "POST",
