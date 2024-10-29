3de1e19d345be6848b38a4709c87f0e9e6d23d17
Lucie Hélias <lucie.helias@protonmail.com>
2024-10-29 10:37:46 +0100

    create future component

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,22 @@ import Header from "./assets/components/Header";
 import Technologies from "./assets/components/Technologies";
 import Projects from "./assets/components/Projects";
 import Contact from "./assets/components/Contact";
+import Future from "./assets/components/Future";
 
 const App = () => {
   return (
-    <div className="overflow-x-hidden text-gray-800 antialiased selection:bg-violet-100 selection:text-violet-800 relative">
-      <div className="fixed top-0 -z-10 h-full w-full">
-        {/* En dessous il y a le background que je dois changer plus tard */}
-        <div className="absolute top-0 -z-10 size-full bg-white ">
-          <div className="absolute bottom-auto left-auto right-0 top-0 size-[500px] translate-x-[-30%] translate-y-[20%] rounded-full bg-[rgba(173,109,244,0.5)] opacity-50 blur-[80px]" />
-        </div>
+    <div className="overflow-x-hidden text-black-800 selection:bg-violet-100 selection:text-violet-800 relative">
+      {/* En dessous il y a le background que je dois changer plus tard */}
+      <div className="top-0 -z-10 size-full bg-white fixed ">
+        <div className="absolute bottom-auto left-auto right-0 top-0 size-[500px] translate-x-[-30%] translate-y-[20%] rounded-full bg-[rgba(173,109,244,0.5)] opacity-50 blur-[80px]" />
       </div>
+
       {/* fin du background */}
+      <Header />
       <div className="container mx-auto px-8">
-        <Header />
         <Hero />
         <About />
+        <Future />
         <Technologies />
         <Projects />
         <Experience />
