b2d8183b43b214b1a09cf7ed39558d86aa857adf
Lucie Hélias <lucie.helias@protonmail.com>
2024-10-29 15:28:10 +0100

    add slider

--- src/assets/components/Projects.jsx
+++ src/assets/components/Projects.jsx
@@ -5,18 +5,19 @@ import { FaLink } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa";
 import { motion } from "framer-motion";
 
+import "../style/slider.css";
+
 const Projects = () => {
   const sliderSettings = {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 1, // Assurez-vous d’afficher une seule image
+    slidesToShow: 1,
     slidesToScroll: 1,
-    arrows: true,
   };
 
   return (
-    <div id="projects">
+    <div id="projects" className="pt-20 pb-4">
       <motion.h1
         whileInView={{ opacity: 1, x: 0 }}
         initial={{ opacity: 0, x: -100 }}
@@ -29,14 +30,17 @@ const Projects = () => {
         initial={{ x: -100, opacity: 0 }}
         animate={{ x: 0, opacity: 1 }}
         transition={{ duration: 0.5, delay: 1.6 }}
-        className="font-light tracking-tighter whitespace-pre-line text-justify text-xl"
+        className="font-light tracking-tighter whitespace-pre-line lg:justify-center mb-32 text-xl lg:px-50"
       >
-        Chaque projet me permet de produire un code propre, testé et
-        réutilisable. Autodidacte, je continue de renforcer mes connaissances en
-        explorant de nouvelles technologies et en m’investissant dans des
-        plateformes de formation comme{" "}
-        <span className="font-bold text-purple-800">Frontend Mentor</span>
-        ou <span className="font-bold text-purple-800">OpenClassrooms</span>.
+        Chaque projet me permet de produire{" "}
+        <span className="font-bold text-purple-800">
+          un code propre, testé et réutilisable
+        </span>
+        . Autodidacte, je continue de renforcer mes connaissances en explorant
+        de nouvelles technologies et en m’investissant dans des plateformes de
+        formation comme{" "}
+        <span className="font-bold text-purple-800">Frontend Mentor</span> et{" "}
+        <span className="font-bold text-purple-800">OpenClassrooms</span>.
       </motion.p>
       <div>
         {PROJECTS.map((project, index) => (
@@ -89,7 +93,6 @@ const Projects = () => {
                   </a>
                 )}
               </div>
-
               <div className="flex flex-wrap gap-2">
                 {project.technologies.map((tech, index) => (
                   <span
@@ -101,8 +104,6 @@ const Projects = () => {
                 ))}
               </div>
             </motion.div>
-
-            {/* Carrousel d'images */}
             {project.image && (
               <motion.div
                 whileInView={{ opacity: 1, x: 0 }}
@@ -111,13 +112,16 @@ const Projects = () => {
                 className="w-full lg:w-1/4"
               >
                 {typeof project.image === "object" ? (
-                  <Slider {...sliderSettings} className="w-full h-full">
+                  <Slider
+                    {...sliderSettings}
+                    className="w-full h-full lg:ml-10"
+                  >
                     {Object.values(project.image).map((imgSrc, imgIndex) => (
                       <img
                         key={imgIndex}
                         src={imgSrc}
                         alt={`${project.title} - Image ${imgIndex + 1}`}
-                        className="object-contain w-full h-64 lg:h-80"
+                        className="object-contain w-full h-64 lg:h-80 "
                       />
                     ))}
                   </Slider>
@@ -130,7 +134,7 @@ const Projects = () => {
                     <img
                       src={project.image}
                       alt={project.title}
-                      className="mt-6 rounded overflow-hidden lg:ml-10 object-cover w-full h-64 h-full"
+                      className="mt-6 rounded object-contain lg:ml-10  "
                     />
                   </a>
                 )}
