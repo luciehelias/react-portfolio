4bb09fc817a42b3d6bc095cca097820dc2418520
Lucie Hélias <lucie.helias@protonmail.com>
2024-10-31 11:52:07 +0100

    Update h1 component

--- src/assets/components/Technologies.jsx
+++ src/assets/components/Technologies.jsx
@@ -80,7 +80,7 @@ const Technologies = () => {
         transition={{ duration: 1.5 }}
         className="my-8 lg:my-20 text-center text-2xl lg:text-5xl"
       >
-        Technologies
+        Mes technologies
       </motion.h1>
       <motion.div
         whileInView={{ opacity: 1, x: 0 }}
