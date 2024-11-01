c698805fb1b91bb6cfa70a92260d148b17599b19
Lucie Hélias <lucie.helias@protonmail.com>
2024-11-01 16:39:20 +0100

    Create title component

---
+++ src/assets/components/Title.jsx
@@ -0,0 +1,11 @@
+const Title = ({ title }) => {
+  return (
+    <div>
+      <h1 className="my-8 lg:my-20 text-center text-2xl lg:text-6xl font-light">
+        {title}
+      </h1>
+    </div>
+  );
+};
+
+export default Title;
